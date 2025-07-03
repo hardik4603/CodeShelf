04a45e9bb4f4b91799e5ffc1516cea04611b49c1
hardik4603 <120990444+hardik4603@users.noreply.github.com>
2025-07-03 09:14:38 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { NavData } from '../utils/navdata'
+import { NavData } from '../utils/NavData'
 
 const Navbar = () => {
   return (
